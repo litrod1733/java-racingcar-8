b0fb0418277c36d9b4059e1c0c4b6ec5ffabd240
inseong <litrod1733@naver.com>
2025-10-24 19:57:02 +0900

    feat: RacingGame, MoveStrategy, RandomMoveStrategy 클래스 뼈대 작성

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,4 +1,19 @@
 package racingcar.controller;
 
+import racingcar.domain.Cars;
+
 public class RacingGame {
+	private final Cars cars;
+	private final int attempts;
+
+	public RacingGame(Cars cars, int attempts) {
+		this.cars = cars;
+		this.attempts = attempts;
+	}
+	public void play() {
+		// 시도 횟수만큼 반복
+	}
+	public Cars getCars() {
+		return cars;
+	}
 }
