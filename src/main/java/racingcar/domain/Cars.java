3a66c0a42e7d4953ff4ec4d67829e381dc323010
inseong <litrod1733@naver.com>
2025-10-24 09:28:47 +0900

    feat(cars): Cars 클래스 초안 작성(fromCsv, fromNames 기본 구조 추가)

--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,4 +1,22 @@
 package racingcar.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Cars {
+	private final List<Car> cars;
+
+	private Cars(String csv) {
+		this.cars = List.copyOf(cars);
+	}
+	public static Cars fromCsv(String csv) {
+		List<String> names = Arrays.stream(csv.split(",")).map(String::trim).toList();
+
+		return fromNames(names);
+	}
+	public static Cars fromNames(List<String> names) {
+		List<Car> cars = names.stream().map(Car::new).toList();
+
+		return new Cars(cars);
+	}
 }
