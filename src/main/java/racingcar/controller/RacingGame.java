2681b44967ef890711e3bc74434eda641b00e8c2
inseong <litrod1733@naver.com>
2025-10-25 01:15:25 +0900

    refactor(racinggame): MoveStrategy 의존성 주입 방식으로 구조 개선

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -1,14 +1,17 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
+import racingcar.domain.strategy.MoveStrategy;
 
 public class RacingGame {
 	private final Cars cars;
 	private final int attempts;
+	private final MoveStrategy strategy;
 
-	public RacingGame(Cars cars, int attempts) {
+	public RacingGame(Cars cars, int attempts, MoveStrategy strategy) {
 		this.cars = cars;
 		this.attempts = attempts;
+		this.strategy = strategy;
 	}
 	public void play() {
 		// 시도 횟수만큼 반복
