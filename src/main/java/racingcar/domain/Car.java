66360bb8d3afd6cfade3ead6552f2b3137fdfc91
inseong <litrod1733@naver.com>
2025-10-25 01:10:05 +0900

    refactor(car): move 메서드 제거 및 moveIf로 이동 로직 일원화

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -20,9 +20,9 @@ public class Car {
 			throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
 		}
 	}
-	public void move(int step) {
-		if (step > 0) {
-			this.position += step;
+	public void moveIf(MoveStrategy strategy) {
+		if (strategy.movable()) {
+			this.position++;
 		}
 	}
 	public String getName() {
@@ -30,11 +30,6 @@ public class Car {
 	}
 	public int getPosition() {
 		return this.position;
-	}
-	public void moveIf(MoveStrategy strategy) {
-		if (strategy.movable()) {
-			this.position++;
-		}
 	}
 	@Override
 	public String toString() {
