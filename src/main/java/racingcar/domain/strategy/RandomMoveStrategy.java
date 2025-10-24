a7c21c04c142acf1099e1e4e42a1748c9a7a4d31
inseong <litrod1733@naver.com>
2025-10-25 02:20:10 +0900

    feat(strategy): 테스트용 변수 수정

--- src/main/java/racingcar/domain/strategy/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/strategy/RandomMoveStrategy.java
@@ -3,8 +3,12 @@ package racingcar.domain.strategy;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
+	private static final int START_NUM = 0;
+	private static final int END_NUM = 9;
+	private static final int MOVE_THRESHOLD = 4;
+
 	@Override
 	public boolean movable() {
-		return Randoms.pickNumberInRange(0, 9) >= 4;
+		return Randoms.pickNumberInRange(START_NUM, END_NUM) >= MOVE_THRESHOLD;
 	}
 }
