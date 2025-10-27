502b11041f3e9f8793e543adb409fad7f2d9a640
inseong <litrod1733@naver.com>
2025-10-27 22:40:10 +0900

    feat(app): Application에서 라운드 루프 수행 및 결과 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,32 @@
 package racingcar;
 
+import racingcar.controller.RacingGame;
+import racingcar.domain.*;
+import racingcar.domain.strategy.RandomMoveStrategy;
+import racingcar.util.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String rawNames = InputView.readNames();
+        Validator.validateNames(List.of(rawNames.split(",")));
+        Cars cars = Cars.fromCsv(rawNames);
+
+        String rawAttempts = InputView.readAttempts();
+        int attempts = Validator.validateAttempts(rawAttempts);
+
+        OutputView.printStart();
+
+        for (int i = 0; i < attempts; i++) {
+            cars.moveAll(new RandomMoveStrategy());
+            OutputView.printRound(cars);
+        }
+
+        List<String> winners = WinnerCalculator.calculate(cars);
+        OutputView.printWinners(winners);
     }
 }
